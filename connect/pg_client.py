65963a8996abbd83341dd0208691047b768e723e
ochsec <chris.ochsenreither@tsgs.com>
2024-05-01 18:51:05 -0700

    updated

--- connect/pg_client.py
+++ connect/pg_client.py
@@ -1,18 +1,17 @@
-import os
 import pg8000
 from dotenv import load_dotenv
 from pg8000.dbapi import ProgrammingError, InterfaceError
 
 class PGClient:
 
-    def __init__(self) -> None:
+    def __init__(self, host, database, password, user='postgres', port=5432) -> None:
         load_dotenv()
         self.db_params = {
-            "host": os.getenv("PG_HOST"),
-            "database": os.getenv("PG_INSTANCE"),
-            "user": os.getenv("PG_USER"),
-            "password": os.getenv("PG_PASSWORD"),
-            "port": os.getenv("PG_PORT")
+            "host": host,
+            "database": database,
+            "user": user,
+            "password": password,
+            "port": port
         }
 
     def get_conn(self):
@@ -27,7 +26,28 @@ class PGClient:
 
         except (ProgrammingError, InterfaceError) as e:
             print(f"Database error occurred: {e}")
+            with self.get_conn() as conn:
+                conn.rollback()
         
         except Exception as e:
             print(f"An error occurred: {e}")
+            with self.get_conn() as conn:
+                conn.rollback()
 
+    def fetch_query(self, query):
+        try:
+            with self.get_conn() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(query)
+                    return cursor.fetchall()
+
+        except (ProgrammingError, InterfaceError) as e:
+            print(f"Database error occurred: {e}")
+            with self.get_conn() as conn:
+                conn.rollback()
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            with self.get_conn() as conn:
+                conn.rollback()
+            
--- connect/sm_client.py
+++ connect/sm_client.py
@@ -1,15 +1,11 @@
-import os
 import boto3
-from dotenv import load_dotenv
 
 class SMClient:
     
     def __init__(self) -> None:
         self.session = boto3.Session()
-        load_dotenv()
-        self.__connect_rds_secret = os.getenv("CONNECT_RDS_SECRET")
         self.__client = self.session.client("secretsmanager")
 
-    def get_rds_password(self):
-        response = self.__client.get_secret_value(SecretId=self.__connect_rds_secret)
+    def get_secret_value(self, secret_name):
+        response = self.__client.get_secret_value(SecretId=secret_name)
         return response["SecretString"]
